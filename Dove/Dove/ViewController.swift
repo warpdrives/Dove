2fa026e2395d2c6f5328caf72bf49a12ddf76128
ShevaKuilin <shevakuilin@gmail.com>
2020-10-29 18:25:07 +0800

    CREAT dove

---
+++ Dove/Dove/ViewController.swift
@@ -0,0 +1,33 @@
+//  Dove
+//
+//  ViewController.swift
+//
+//  Copyright (c) 2020-Present Warpdrives Team - https://github.com/warpdrives
+//
+//  The software is available under the Apache 2.0 license. See the LICENSE file
+//  for more info.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+//  Created by ShevaKuilin on 2020/10/29.
+//  Copyright © 2020 Warpdrives Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Dove/DoveTests/DoveTests.swift
@@ -0,0 +1,34 @@
+//
+//  DoveTests.swift
+//  DoveTests
+//
+//  Created by ShevaKuilin on 2020/10/29.
+//  Copyright © 2020 Warpdrives Team. All rights reserved.
+//
+
+import XCTest
+@testable import Dove
+
+class DoveTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
