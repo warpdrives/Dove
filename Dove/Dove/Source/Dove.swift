7f5f47a6435e2e232f53647c7afe70bc0108a267
ShevaKuilin <shevakuilin@gmail.com>
2020-10-30 15:41:16 +0800

    CREATE: dove.swift

---
+++ Dove/Dove/Source/Dove.swift
@@ -0,0 +1,48 @@
+//  Dove
+//
+//  Dove.swift
+//
+//  Copyright (c) 2020-Present Warpdrives Team - https://github.com/warpdrives
+//
+//  The software is available under the Apache 2.0 license. See the LICENSE file
+//  for more info.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+//  Created by ShevaKuilin on 2020/10/30.
+//  Copyright © 2020 Warpdrives Team. All rights reserved.
+//
+
+import UIKit
+
+private struct Pool {
+    var reusablePool: Set<UIView>!
+    var visiablePool: Set<UIView>!
+}
+
+class Dove: UIView {
+    private var containScrollView: UIScrollView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
